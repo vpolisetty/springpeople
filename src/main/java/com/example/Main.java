7fa0b753af0413b4763f47d1b2eca56f63c97ee2
viresh <virkumar@microsoft.com>
2020-05-17 22:14:54 +0530

    config example

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,8 +16,18 @@
 
 package com.example;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.measure.quantity.Mass;
+import javax.sql.DataSource;
+
+import org.jscience.physics.amount.Amount;
+import org.jscience.physics.model.RelativisticModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -26,13 +36,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import static javax.measure.unit.SI.KILOGRAM;
 
 @Controller
 @SpringBootApplication
@@ -83,6 +89,18 @@ public class Main {
       config.setJdbcUrl(dbUrl);
       return new HikariDataSource(config);
     }
+  }
+  
+  @RequestMapping("/hello")
+  String hello(Map<String, Object> model) {
+      RelativisticModel.select();
+      String energy = System.getenv().get("ENERGY");
+      if (energy == null) {
+         energy = "12 GeV";
+      }
+      Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+      model.put("science", "E=mc^2: 12 GeV = " + m.toString());
+      return "hello";
   }
 
 }
